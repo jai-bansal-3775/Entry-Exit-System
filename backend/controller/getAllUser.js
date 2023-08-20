bba94ea29658cbd838ca31f2dc55612dd8f5dfa5
Dhiraj-Agarwal-IITG <dhirajagarwal2018@gmail.com>
2023-08-20 11:27:09 +0530

    new file added

---
+++ backend/controller/getAllUser.js
@@ -0,0 +1,12 @@
+const Register = require("../models/User");
+exports.getAllUser = async (req, res) => {
+
+	try {
+		const userData = await Register.find({});
+		console.log("Printing the User Data : ",userData);
+		res.json({ success: true, data: userData });
+	} catch (error) {
+		console.log("input data is wrong")
+		res.status(500).json({ success: false, error: error,message:"Process Failed"});
+	}
+};
