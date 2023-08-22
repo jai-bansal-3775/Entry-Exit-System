0c3e630ff627c9705e2800f3b4edc47e46b98c32
Dhiraj-Agarwal-IITG <dhirajagarwal2018@gmail.com>
2023-08-23 03:58:53 +0530

    Adding bug fixed

---
+++ backend/controller/getUserFromRegister.js
@@ -0,0 +1,15 @@
+const Register = require("../models/Register");
+exports.getUserFromRegister = async (req, res) => {
+
+	const roll = req.params.id;
+	console.log("Printing Roll No in getallUser  :",roll);
+
+	try {
+		const userData = await Register.find({rollNo:roll,inTime:{$exists:false}});
+		console.log("Printing the User Data : ",userData);
+		res.json({ success: true, data: userData });
+	} catch (error) {
+		console.log("input data is wrong")
+		res.status(500).json({ success: false, error: error,message:"Process Failed"});
+	}
+};
